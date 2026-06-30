1b1facf76a8d550ead1185f135ead9f3f1c54e61
heliparacelse-netizen <heliparacelse@gmail.com>
2026-06-30 22:15:37 +0100

    Delete Roomvera directory

--- Roomvera/src/config/stripe.js
+++
@@ -1,9 +0,0 @@
-const Stripe = require('stripe');
-
-// Stripe est optionnel tant que le paiement n'est pas active.
-// Sans cle, on n'instancie pas le client (evite un crash au demarrage sur Render).
-const stripe = process.env.STRIPE_SECRET_KEY
-  ? new Stripe(process.env.STRIPE_SECRET_KEY)
-  : null;
-
-module.exports = stripe;
